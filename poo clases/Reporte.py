9e90a84e086b6e2e560c4293fc052149d649a1e3
jeidy_ver <verajeidy36@gmail.com>
2026-06-16 16:43:43 -0600

    comentar clase Reporte.py

--- poo clases/Reporte.py
+++ poo clases/Reporte.py
@@ -2,12 +2,9 @@ from datetime import datetime
 
 class Reporte:
 
-    def __init__(self,
-                 tipo,
-                 fecha_generacion=None):
-
+    def __init__(self, tipo, fecha_generacion=None):
         self.tipo = tipo
-
+# Genera automáticamente la fecha actual si no se proporciona una
         if fecha_generacion is None:
             self.fecha_generacion = datetime.now().strftime(
                 "%d/%m/%Y %H:%M:%S"
@@ -18,91 +15,44 @@ class Reporte:
             self,
             estudiantes):
 
-        print("\n===================================")
         print("REPORTE DE ESTUDIANTES")
-        print("===================================")
-        print(
-            f"Fecha: {self.fecha_generacion}"
-        )
+        print(f"Fecha: {self.fecha_generacion}")
 
         if len(estudiantes) == 0:
-            print("\nNo existen estudiantes registrados")
+            print("No existen estudiantes registrados")
             return
-
         print()
-
         for estudiante in estudiantes:
-
-            print(
-                f"ID: {estudiante[0]}"
-            )
-
-            print(
-                f"Nombre: {estudiante[1]}"
-            )
-
-            print(
-                f"Modalidad: {estudiante[9]}"
-            )
-
-            print(
-                f"Estado: {estudiante[8]}"
-            )
-
+            print(f"ID: {estudiante[0]}")
+            print(f"Nombre: {estudiante[1]}")
+            print(f"Modalidad: {estudiante[9]}")
+            print(f"Estado: {estudiante[8]}")
             print("-----------------------------------")
 
-    def generar_reporte_profesores(
-            self,
-            profesores):
+    def generar_reporte_profesores(self, profesores):
 
-        print("\n===================================")
         print("REPORTE DE PROFESORES")
-        print("===================================")
-        print(
-            f"Fecha: {self.fecha_generacion}"
-        )
+        print(f"Fecha: {self.fecha_generacion}")
 
         if len(profesores) == 0:
-            print("\nNo existen profesores registrados")
+            print("No existen profesores registrados")
             return
-
         print()
 
         for profesor in profesores:
-
-            print(
-                f"ID: {profesor[0]}"
-            )
-
-            print(
-                f"Nombre: {profesor[1]}"
-            )
-
-            print(
-                f"Materia: {profesor[6]}"
-            )
-
-            print(
-                f"Título: {profesor[7]}"
-            )
-
+            print(f"ID: {profesor[0]}")
+            print(f"Nombre: {profesor[1]}")
+            print(f"Materia: {profesor[6]}")
+            print(f"Título: {profesor[7]}")
             print("-----------------------------------")
 
-    def generar_reporte_general(
-            self,
-            estudiantes,
-            profesores):
-
-        print("\n===================================")
+    def generar_reporte_general(self, estudiantes, profesores):
         print("REPORTE GENERAL")
-        print("===================================")
+
+        print(f"Fecha: {self.fecha_generacion}")
 
         print(
-            f"Fecha: {self.fecha_generacion}"
-        )
-
-        print(
-            f"\nTotal estudiantes: "
+            f"Total estudiantes: "
             f"{len(estudiantes)}"
         )
 

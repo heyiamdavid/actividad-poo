8e78dbc2de4cd903054edebdb67f49d8d0fd32c3
salchivann <vanessamol3006@gmail.com>
2026-06-16 18:25:58 -0500

    Clase Profesor comentada

--- poo clases/Profesor.py
+++ poo clases/Profesor.py
@@ -2,6 +2,7 @@ from Persona import Persona
 
 class Profesor(Persona):
 
+    # Herencia y reutilización de código mediante super()
     def __init__(self,
                  nombre,
                  telefono,
@@ -21,10 +22,11 @@ class Profesor(Persona):
 
         self.materia = materia
         self.titulo = titulo
-
+    #Encapsulacion en las listas
         self.__notas = []
         self.__evaluaciones = []
 
+    # Encapsulación
     @property
     def notas(self):
         return self.__notas
@@ -33,6 +35,8 @@ class Profesor(Persona):
     def evaluaciones(self):
         return self.__evaluaciones
 
+    # Abstracción
+    # Permite registrar notas ocultando la lógica interna.
     def registrar_nota(self, nota):
 
         if 0 <= nota <= 10:
@@ -44,6 +48,8 @@ class Profesor(Persona):
         else:
             print("Nota inválida")
 
+    # Asociación
+    # Un profesor puede crear y administrar objetos Evaluacion.
     def crear_evaluacion(self, evaluacion):
 
         self.__evaluaciones.append(evaluacion)
@@ -52,6 +58,8 @@ class Profesor(Persona):
             f"Evaluación {evaluacion.nombreEvaluacion} registrada"
         )
 
+    # Asociación
+    # Interactúa con una colección de objetos Estudiante.
     def listar_estudiantes(self, estudiantes):
 
         print("\n========== ESTUDIANTES ==========")
@@ -67,6 +75,8 @@ class Profesor(Persona):
                 f"{estudiante.identificacion}"
             )
 
+    # Polimorfismo (Sobrescritura de método), se redefine mostrar_datos() heredado de Persona,
+    # agregando información específica del profesor.
     def mostrar_datos(self):
 
         super().mostrar_datos()

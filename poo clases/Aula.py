81c330172640cb474f87ac6869e537be14a5e1db
jeidy_ver <verajeidy36@gmail.com>
2026-06-16 16:01:47 -0600

    comentar clase Aula.py

--- poo clases/Aula.py
+++ poo clases/Aula.py
@@ -1,12 +1,10 @@
+#Esta clase representa un aula dentro del sistema universitario
 class Aula:
-
     def __init__(self,
                  numeroAula,
                  capacidad):
-
         self.numeroAula = numeroAula
-
-        self.__capacidad = capacidad
+        self.__capacidad = capacidad #atributo encapsulado
 
     @property
     def capacidad(self):
@@ -14,7 +12,6 @@ class Aula:
 
     @capacidad.setter
     def capacidad(self, nueva_capacidad):
-
         if nueva_capacidad > 0:
             self.__capacidad = nueva_capacidad
         else:
@@ -22,7 +19,6 @@ class Aula:
 
     # MÉTODOS
     def mostrar_aula(self):
-
         print(
             f"Aula {self.numeroAula} "
             f"(Capacidad: {self.__capacidad})"

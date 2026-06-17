d9cc0b46a24893100c8e98ffd62216c6f692da6a
stvvnn <stiven.simba06@gmail.com>
2026-06-16 19:59:48 -0500

    FabricaPersona

--- poo clases/FabricaPersona.py
+++ poo clases/FabricaPersona.py
@@ -1,31 +1,68 @@
 from abc import ABC, abstractmethod
 
-from Curso import Curso
-from CursoNivelacion import CursoNivelacion
-from Evaluacion import Evaluacion
+from Estudiante import Estudiante
+from Profesor import Profesor
 
-class FabricaUniversitaria(ABC):
-    @abstractmethod
-    def crear_curso(self, nombre, identificador, extra):
-        pass
+#Factory Method 
+class FabricaPersona(ABC):
 
     @abstractmethod
-    def crear_evaluacion(self, nombre, calificacion):
+    def crear_persona(self, *args):
         pass
 
-class FabricaRegimenRegular(FabricaUniversitaria):
-    #Fábrica concreta para cursos del régimen regular
 
-    def crear_curso(self, nombre, identificador, extra):
-        return Curso(codigoCurso=identificador, nombreCurso=nombre, creditos=int(extra))
+class FabricaEstudiante(FabricaPersona):
 
-    def crear_evaluacion(self, nombre, calificacion):
-        return Evaluacion(nombreEvaluacion=nombre, calificacion=float(calificacion))
-        
-class FabricaRegimenNivelacion(FabricaUniversitaria):
-    #Fábrica concreta para cursos de nivelación
-    def crear_curso(self, nombre, identificador, extra):
-        return CursoNivelacion(nombreCurso=nombre, paralelo=identificador, profesor=extra)
+    def crear_persona(
+        self,
+        nombre,
+        telefono,
+        email,
+        identificacion,
+        contrasena,
+        promedio_ingreso,
+        promedio_graduacion,
+        estado,
+        modalidad
+    ):
 
-    def crear_evaluacion(self, nombre, calificacion):
-        return Evaluacion(nombreEvaluacion=nombre, calificacion=float(calificacion))
+        return Estudiante(
+
+            nombre,
+            telefono,
+            email,
+            identificacion,
+            contrasena,
+
+            promedio_ingreso,
+            promedio_graduacion,
+
+            estado,
+            modalidad
+        )
+
+
+class FabricaProfesor(FabricaPersona):
+
+    def crear_persona(
+        self,
+        nombre,
+        telefono,
+        email,
+        identificacion,
+        contrasena,
+        materia,
+        titulo
+    ):
+
+        return Profesor(
+
+            nombre,
+            telefono,
+            email,
+            identificacion,
+            contrasena,
+
+            materia,
+            titulo
+        )

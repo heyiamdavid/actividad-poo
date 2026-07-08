86df028629d693ad7e39b0bfb6401fd6123e7552
eliikawa <juliealcivar7@gmail.com>
2026-07-07 21:30:57 -0500

    Add comments for repository instantiation and database connection
    Added comments to clarify the instantiation of the repository and its connection to the local database.

--- POOproyecto/Main.py
+++ POOproyecto/Main.py
@@ -1,7 +1,8 @@
 from RepositorioSQLite import RepositorioSQLite
 from Administrador import Administrador
 from Sistema import Sistema
-
+# Se instancia el repositorio que manejará la persistencia de datos, 
+# conectándolo a la base de datos local 'universidad.db'
 if __name__ == "__main__":
     repositorio = RepositorioSQLite("universidad.db")
     administrador = Administrador(repositorio) #inyección de dependencias
